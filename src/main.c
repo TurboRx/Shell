08574a492e17ea2f328cf38e8e19547df970af95
TurboRx <187360786+TurboRx@users.noreply.github.com>
2026-08-18 07:40:06 +0000

    Add basic shell

---
+++ src/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+  // Flush after every printf
+  setbuf(stdout, NULL);
+  
+  printf("$ ");
+
+  char input[100];
+  fgets(input, 100, stdin);
+
+  input[strlen(input) - 1] = '\0';
+  printf("%s: command not found\n", input);
+  
+  return 0;
+}
